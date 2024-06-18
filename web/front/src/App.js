525dc0f3509727fb8ab9883e18261ddc5348ab86
GangBean <xbox2537@gmail.com>
2024-06-18 10:06:10 +0900

    feat: add signup button component #12

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, World!</h1>
+        </header>
+      </div>
+      <div className="signUp">
+        <button className="signUp-button">회원가입</button>
+      </div>
     </div>
   );
 }
