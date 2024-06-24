32f9818b448d7c40aa91ff688db325603bba4f2f
GangBean <xbox2537@gmail.com>
2024-06-24 11:40:19 +0900

    feat: implements google login API #12

--- web/back/src/main/java/com/mybasket/web/controller/UserController.java
+++ web/back/src/main/java/com/mybasket/web/controller/UserController.java
@@ -3,9 +3,19 @@ package com.mybasket.web.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 
 
@@ -16,11 +26,14 @@ public class UserController {
         @Value("${auth.google.client.id}")
         private String clientId;
 
+        @Value("${auth.google.client.secret}")
+        private String clientSecret;
+
         @Value("${auth.google.client.redirectUri}")
         private String redirectUri;
-
-        @GetMapping("/member")
-        public ResponseEntity<String> signUp() {
+        
+	@GetMapping("/")
+        public ResponseEntity<String> redirct_google_consent() {
             System.out.println("SignUp");
             String googleAuthUrl = "https://accounts.google.com/o/oauth2/v2/auth"
                 + "?client_id=" + clientId
@@ -29,10 +42,48 @@ public class UserController {
                 + "&scope=profile%20email"
                 + "&access_type=offline";
 
-            return new ResponseEntity(googleAuthUrl, HttpStatus.TEMPORARY_REDIRECT);
+            return ResponseEntity
+		    .status(HttpStatus.FOUND)
+		    .header("Location", googleAuthUrl)
+		    .build();
         }
 
-        @GetMapping("/")
+        @GetMapping("/members")
+        public ResponseEntity<Map> login(@RequestParam("code") String code) {
+            RestClient client = RestClient.create();            
+            
+            MultiValueMap<String, String> request = new LinkedMultiValueMap<>();
+            request.add("code", code); 
+            request.add("client_id", clientId);
+            request.add("client_secret",clientSecret);
+            request.add("redirect_uri", redirectUri);
+            request.add("grant_type", "authorization_code");
+
+            ResponseEntity<Map> response = client.post()
+		    .uri("https://oauth2.googleapis.com/token")
+		    .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+		    .body(request)
+		    .retrieve()
+		    .toEntity(Map.class);
+	    String accessToken = (String) response.getBody().get("access_token");
+            
+            // request to get userinfo
+            String userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+            response = client.get()
+		    .uri(userInfoUrl)
+		    .headers(header -> header.setBearerAuth(accessToken))
+		    .retrieve()
+		    .toEntity(Map.class);
+	    Map userInfo = (Map) response.getBody();
+
+	    // String email = userInfo.get("email");
+            // enroll app member and login
+            // memberService.signupAndLogin(email);
+
+	    return response;
+        }
+
+        @GetMapping("/hello")
         public String hello() {
             System.out.println("hello");
             return "hello";
