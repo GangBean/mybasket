1da40b1a5f4708b1937c62b8724d34c815d973d5
GangBean <xbox2537@gmail.com>
2024-07-07 13:05:11 +0900

    test: add MemberHistory valid test #20

--- web/front/test/body/Sections.test.jsx
+++ web/front/test/body/Sections.test.jsx
@@ -3,23 +3,28 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import Sections from "../../src/body/Sections";
 
-describe("Sections component test", () => { 
-    test("MemberHistory component를 포함합니다.", ()=>{
-        const { container } = render(<Sections></Sections>);
+describe("Sections component test", () => {
+    test("로그인 상태일때 MemberHistory component를 포함합니다.", () => {
+        const { container } = render(<Sections isLoggedIn={true}></Sections>);
 
         expect(container.querySelector(".memberHistory")).toBeInTheDocument();
     });
-    test("YesterdayRanking component를 포함합니다.", ()=>{
+    test("로그아웃 상태일때 MemberHistory component를 포함하지 않습니다.", () => {
+        const { container } = render(<Sections isLoggedIn={false}></Sections>);
+
+        expect(container.querySelector(".memberHistory")).not.toBeInTheDocument();
+    });
+    test("YesterdayRanking component를 포함합니다.", () => {
         const { container } = render(<Sections></Sections>);
 
         expect(container.querySelector(".yesterdayRanking")).toBeInTheDocument();
     });
-    test("AllTimeRanking component를 포함합니다.", ()=>{
+    test("AllTimeRanking component를 포함합니다.", () => {
         const { container } = render(<Sections></Sections>);
 
         expect(container.querySelector(".allTimeRanking")).toBeInTheDocument();
     });
-    test("RandomRecommendation component를 포함합니다.", ()=>{
+    test("RandomRecommendation component를 포함합니다.", () => {
         const { container } = render(<Sections></Sections>);
 
         expect(container.querySelector(".randomRecommendation")).toBeInTheDocument();
