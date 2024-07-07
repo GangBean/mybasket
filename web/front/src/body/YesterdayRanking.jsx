377843282293572977448a0436e1d0fd2ddb1779
GangBean <xbox2537@gmail.com>
2024-07-07 14:07:47 +0900

    feat: implements YesterdayRanking component #20

--- web/front/src/body/YesterdayRanking.jsx
+++ web/front/src/body/YesterdayRanking.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import RankingRecipe from "./RankingRecipe";
+import MoreButton from "./MoreButton";
 
-const YesterdayRanking = () => {
+const YesterdayRanking = ({ getNext }) => {
+    const [recipes, setRecipes] = useState([]);
     return (
-        <div className="yesterdayRanking"></div>
+        <div className="yesterdayRanking">
+            <RankingRecipes recipes={recipes}></RankingRecipes>
+            <MoreButton recipes={recipes} setRecipes={setRecipes} getNext={getNext}></MoreButton>
+        </div>
+    );
+};
+
+const RankingRecipes = ({ recipes }) => {
+    return (
+        <ol className="rankingRecipes">
+            {recipes.map(recipe => <RankingRecipe key={recipe.recipeId} recipe={recipe}></RankingRecipe>)}
+        </ol>
     );
 };
 
--- web/front/test/body/YesterdayRanking.test.jsx
+++ web/front/test/body/YesterdayRanking.test.jsx
@@ -11,18 +11,18 @@ describe("YesterdayRanking component test", () => {
     });
     test("MoreButton을 갖습니다.", () => {
         const { container } = render(<YesterdayRanking></YesterdayRanking>);
-        
+
         expect(container.querySelector(".moreButton")).toBeInTheDocument();
     });
     test("더보기 버튼을 클릭하면 한번에 최대 10개의 레시피가 추가됩니다.", async () => {
-        const value = Array.from({length: 10}).map((_, x) => {
-            return {recipeId: x};
+        const value = Array.from({ length: 10 }).map((_, x) => {
+            return { recipeId: x };
         });
         const getNext = () => {
             return value;
         };
         const { container } = render(<YesterdayRanking getNext={getNext}></YesterdayRanking>);
-        
+
         const moreButton = container.querySelector(".moreButton");
         await fireEvent.click(moreButton);
 
