b26e44566aaa04c9dd59b0b73a4c362e8b4d2e59
GangBean <xbox2537@gmail.com>
2024-07-07 12:13:30 +0900

    feat: implements BasketSection component #20

--- web/front/src/footer/BasketSection.jsx
+++ web/front/src/footer/BasketSection.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MyBasket from "./MyBasket";
 
-const BasketSection = ({ isLoggedIn }) => {
+const MAX_MYBASKET_LENGTH = 5;
+
+const BasketSection = ({ isLoggedIn, myBaskets }) => {
     return (
         <div className="basketSection">
-            {isLoggedIn ? <MemberMode /> : <GuestMode />}
+            {isLoggedIn ? <MemberMode myBaskets={myBaskets} /> : <GuestMode />}
         </div>
     );
 };
@@ -21,11 +23,10 @@ const LoginInstruction = () => {
     return <p className="loginInstruction">서비스를 이용하기 위해 로그인이 필요합니다.</p>;
 }
 
-const MemberMode = () => {
-    const myBaskets = [];
+const MemberMode = ({ myBaskets }) => {
     return (
         <div className="memberMode">
-            <MyBaskets myBaskets={myBaskets}/>
+            <MyBaskets myBaskets={myBaskets} />
         </div>
     );
 };
@@ -33,7 +34,7 @@ const MemberMode = () => {
 const MyBaskets = ({ myBaskets }) => {
     return (
         <ol className="myBaskets">
-            {myBaskets.map(myBasket => <MyBasket />)}
+            {myBaskets.slice(0, MAX_MYBASKET_LENGTH).map(myBasket => <MyBasket key={myBasket.myBasketId} myBasket={myBasket} />)}
         </ol>
     )
 }
