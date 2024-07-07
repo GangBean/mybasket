eea98f83dbe7da93aba346c919d0c050d8103a0c
GangBean <xbox2537@gmail.com>
2024-07-07 14:25:16 +0900

    feat: implements RandomRecommendation component #20

--- web/front/src/body/RandomRecommendation.jsx
+++ web/front/src/body/RandomRecommendation.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import MyRecipe from "./MyRecipe";
+import MoreButton from "./MoreButton";
 
-const RandomRecommendation = () => {
+const RandomRecommendation = ({ getNext }) => {
+    const [myRecipes, setMyRecipes] = useState([]);
     return (
-        <div className="randomRecommendation"></div>
+        <div className="randomRecommendation">
+            <MyRecipes myRecipes={myRecipes}></MyRecipes>
+            <MoreButton recipes={myRecipes} setRecipes={setMyRecipes} getNext={getNext}></MoreButton>
+        </div>
+    );
+};
+
+const MyRecipes = ({ myRecipes }) => {
+    return (
+        <ol className="myRecipes">
+            {myRecipes.map(myRecipe => <MyRecipe key={myRecipe.myRecipeId} myRecipe={myRecipe}></MyRecipe>)}
+        </ol>
     );
 };
 
