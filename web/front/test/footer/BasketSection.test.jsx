13a31d8b015f6db2b7a2748f86b99ee60f8700ff
GangBean <xbox2537@gmail.com>
2024-07-07 12:12:27 +0900

    test: add MyBaskets length limit validation test #20

--- web/front/test/footer/BasketSection.test.jsx
+++ web/front/test/footer/BasketSection.test.jsx
@@ -17,14 +17,74 @@ describe("BasketSection component test", () => {
     });
 
     test("로그인 상태에서는 MemberMode를 갖습니다.", () => {
-        const { container } = render(<BasketSection isLoggedIn={true} />);
+        const { container } = render(<BasketSection isLoggedIn={true} myBaskets={[]}/>);
 
         expect(container.querySelector('.memberMode')).toBeInTheDocument();
     });
 
     test("로그인 상태에서는 MyBaskets를 갖습니다.", () => {
-        const { container } = render(<BasketSection isLoggedIn={true} />);
+        const { container } = render(<BasketSection isLoggedIn={true} myBaskets={[]}/>);
 
         expect(container.querySelector('.myBaskets')).toBeInTheDocument();
+    });
+
+    test("MyBaskets는 최대 5개의 리스트만 출력합니다.", () => {
+        const input = [
+            {
+                myBasketId: 1,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 2,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 3,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 4,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 5,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 6,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 7,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 8,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 9,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+            {
+                myBasketId: 10,
+                myBasketTime: new Date(),
+                myBasketBudget: 10000,
+            },
+        ];
+        const { container } = render(<BasketSection isLoggedIn={true} myBaskets={input} />);
+
+        const myBaskets = container.querySelector('.myBaskets').querySelectorAll('li');
+
+        expect(myBaskets.length).toBe(5);
     });
 });
