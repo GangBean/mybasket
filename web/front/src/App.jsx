e6496462b33daff602b90346fffae4d92b7e8497
GangBean <xbox2537@gmail.com>
2024-07-08 13:23:33 +0900

    refactor: extract getSessionState function #20

--- web/front/src/App.jsx
+++ web/front/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [page, setPage] = useState(Pages.MAIN);
 
-  useEffect(getSessionState, []);
+  useEffect(() => getSessionState(setIsLoggedIn), []);
 
   return (
     // <div>
@@ -48,22 +48,22 @@ const PageButton = ({ page, setPage }) => {
   return (
     <form>
       <label>
-        <input type='radio' value={Pages.MAIN} checked={page===Pages.MAIN} onChange={handleChange}></input>
+        <input type='radio' value={Pages.MAIN} checked={page === Pages.MAIN} onChange={handleChange}></input>
         MAIN
       </label>
       <label>
-        <input type='radio' value={Pages.LOGIN} checked={page===Pages.LOGIN} onChange={handleChange}></input>
+        <input type='radio' value={Pages.LOGIN} checked={page === Pages.LOGIN} onChange={handleChange}></input>
         LOGIN
       </label>
       <label>
-        <input type='radio' value={Pages.MY_PAGE} checked={page===Pages.MY_PAGE} onChange={handleChange}></input>
+        <input type='radio' value={Pages.MY_PAGE} checked={page === Pages.MY_PAGE} onChange={handleChange}></input>
         MY_PAGE
       </label>
     </form>
   );
 };
 
-const getSessionState = () => {
+const getSessionState = ({ setIsLoggedIn }) => {
   axios.get('http://localhost:8080/api/auth/sessions', { withCredentials: true })
     .then(response => {
       if (response.data.loggedIn) {
