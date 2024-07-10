0de5bdbbe9fc0bae858f1a6750324c0bfd622cbf
GangBean <xbox2537@gmail.com>
2024-07-11 07:53:22 +0900

    refactor: extract YesterdayRankingBox and Title component #20

---
+++ web/front/src/body/YesterdayRankingTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const YesterdayRankingTitle = () => {
+    return (
+        <div className="yesterdayRankingTitle">
+            <h2>어제의 레시피</h2>
+        </div>
+    );
+};
+
+export default YesterdayRankingTitle;
